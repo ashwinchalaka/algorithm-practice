c491b0d816828f582660798bc1a98e8c8e5b87a5
Ashwin Chalaka <ashesmachine@Ashwins-MacBook-Pro-2.local>
2020-02-23 18:46:40 -0800

    2Darray3-PascalsTriangle -- finished another one, this one was fun too

--- arrays_and_strings/2Darray3_PascalsTriangle/2Darray3-PascalsTriangle.java
+++ arrays_and_strings/2Darray3_PascalsTriangle/2Darray3-PascalsTriangle.java
@@ -1,16 +1,4 @@
 class Solution {
-    
-    private static String rowToString(List<Integer> arrayList) {
-        
-        String str = "";
-        
-        for (int i = 0 ; i < arrayList.size(); i++) {
-            str += arrayList.get(i) + ", ";
-        }
-        
-        return str;        
-    }
-    
     public List<List<Integer>> generate(int numRows) {
         
         List<List<Integer>> pascalsTriangle = new ArrayList<>();
@@ -22,39 +10,25 @@ class Solution {
         triangleRow.add(1);
         pascalsTriangle.add(triangleRow);
 
-        if (numRows == 1)
-            return pascalsTriangle;
-
-        triangleRow = new ArrayList<>();
-        triangleRow.add(1);
-        triangleRow.add(1);
-        pascalsTriangle.add(triangleRow);
-
-        if (numRows == 2)
-            return pascalsTriangle;
-
         List<Integer> previousRow = triangleRow;
-        int firstHidden = 0;
-        int lastHidden = 0;
             
-        for (int row = 3; row <= numRows; row++) {
+        for (int row = 1; row < numRows; row++) {
             triangleRow = new ArrayList<>();
             
-            triangleRow.add(firstHidden + previousRow.get(0));
+            triangleRow.add(1);
             
             for (int pointer = 0; pointer < previousRow.size()-1; pointer++) {
                 int newSum = previousRow.get(pointer) + previousRow.get(pointer+1);
                 triangleRow.add(newSum);
             }
             
-            triangleRow.add(lastHidden + previousRow.get(previousRow.size()-1));
+            triangleRow.add(1);
             
             pascalsTriangle.add(triangleRow);
             previousRow = triangleRow;
         }
 
         return pascalsTriangle;
-        
         
     }
 }
