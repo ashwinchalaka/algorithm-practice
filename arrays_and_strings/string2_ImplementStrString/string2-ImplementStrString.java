f87c69d354b047cedf7ca08557964175a34c407d
Ashwin Chalaka <ashesmachine@Ashwins-MacBook-Pro-2.local>
2020-02-26 14:40:29 -0800

    String2-ImplementStrString -- FINISHED

--- arrays_and_strings/string2_ImplementStrString/string2-ImplementStrString.java
+++ arrays_and_strings/string2_ImplementStrString/string2-ImplementStrString.java
@@ -1,31 +1,39 @@
 class Solution {
     public int strStr(String haystack, String needle) {
         
-        if (needle.compareTo("") == 0)
+        boolean etch1 = needle.length() == 0;
+        boolean etch2 = haystack.compareTo(needle) == 0;
+        
+        if (etch1 || etch2)
             return 0;
         
-        int lengthDiff = haystack.length() - needle.length();
+        etch1 = haystack.length() == 0 && needle.length() > 0;
+        etch2 = haystack.length() < needle.length();
         
-        if (lengthDiff == 0) {
-            if (haystack.compareTo(needle) == 0)
-                return 0;
+        if (etch1 || etch2)
+            return -1;
+        
+        int haystackPointer = 0;
+        int lengthDiff = haystack.length() - needle.length();
+
+        String subStrToCompare = haystack.substring(
+            haystackPointer, haystackPointer+needle.length());
+
+        while (haystackPointer < lengthDiff) {
+
+            if (subStrToCompare.compareTo(needle) == 0)
+                return haystackPointer;
+
+            haystackPointer++;
+            subStrToCompare = haystack.substring(haystackPointer, haystackPointer+needle.length());
+        }
+
+        if (haystackPointer+needle.length() > haystack.length()) {
+            return -1;
         } else {
-            
-            int haystackPointer = 0;
-            String subStrToCompare;
-            
-            while (haystackPointer < haystack.length()-lengthDiff) {
-                subStrToCompare = haystack.substring(haystackPointer, haystackPointer+needle.length());
-                System.out.println(subStrToCompare);
-                if (subStrToCompare.compareTo(needle) == 0)
-                    return haystackPointer;
-                haystackPointer++;
-            }
-            
-            // subStrToCompare = haystack.substring(haystackPointer, haystackPointer+needle.length());
-            // System.out.println(subStrToCompare);
-            // if (subStrToCompare.compareTo(needle) == 0)
-            //     return haystackPointer;
+            subStrToCompare = haystack.substring(haystackPointer, haystackPointer+needle.length());
+            if (subStrToCompare.compareTo(needle) == 0)
+                return haystackPointer;
         }
         
         return -1;        
