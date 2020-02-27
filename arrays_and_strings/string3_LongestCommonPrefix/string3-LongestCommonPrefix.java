13a2ae16cff70c666ec20065ace1a64696f5d766
Ashwin Chalaka <ashesmachine@Ashwins-MacBook-Pro-2.local>
2020-02-27 15:43:58 -0800

    String3-LongestCommonPrefix.java -- FINISHED, working solution

--- arrays_and_strings/string3_LongestCommonPrefix/string3-LongestCommonPrefix.java
+++ arrays_and_strings/string3_LongestCommonPrefix/string3-LongestCommonPrefix.java
@@ -1,41 +1,47 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         
-        if (strs.length == 0)
+        boolean etch1 = strs.length == 0;
+        boolean etch2 = strs.length == 1;
+        
+        if (etch1)
             return "";
         
-        if (strs.length == 1)
+        if (etch2)
             return strs[0];
         
-        String longestCommonPrefix = strs[0];
-        int strPointer = strs[0].length()-1;
+        int strPointer = 0;
+        String currentPrefix = strs[0].substring(0,strPointer);
         
         int matchCount = 0;
-        
-        System.out.println("strs length = " + strs.length);
+        String longestCommonPrefix = "";
         
         for (int i = 0; i < strs[0].length(); i++) {
             
-            longestCommonPrefix = strs[0].substring(0,strPointer);
-            System.out.println(longestCommonPrefix);
+            if (strPointer < strs[0].length())
+                strPointer++;
+            else 
+                break;
+            
+            currentPrefix = strs[0].substring(0,strPointer);
+            System.out.println(currentPrefix);
+            
             matchCount = 0;
                 
             for (int j = 0; j < strs.length; j++) {
-                
-                
-                
-                if (longestCommonPrefix.compareTo(strs[j]) == 0)
-                    matchCount++;
+                if (strPointer <= strs[j].length()) {
+                    String nextElementPrefix = strs[j].substring(0,strPointer);
+
+                    if (currentPrefix.compareTo(nextElementPrefix) == 0)
+                        matchCount++;                    
+                } else
+                    return longestCommonPrefix;
             }
             
-            System.out.println("matchCount = " + matchCount);
-            
             if (matchCount == strs.length)
-                return longestCommonPrefix;
-            
-            strPointer--;
+                longestCommonPrefix = currentPrefix;
         }
         
-        return "";
+        return longestCommonPrefix;
     }
 }
