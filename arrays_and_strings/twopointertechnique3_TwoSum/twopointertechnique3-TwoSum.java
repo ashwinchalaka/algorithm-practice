654d5b954d93e312b0522c00fce20272ffc35803
Ashwin Chalaka <ashesmachine@Ashwins-MacBook-Pro-2.local>
2020-02-27 16:56:45 -0800

    twopointertechnique3-TwoSum.java -- FINISHED, working solution, second trial did not work

--- arrays_and_strings/twopointertechnique3_TwoSum/twopointertechnique3-TwoSum.java
+++ arrays_and_strings/twopointertechnique3_TwoSum/twopointertechnique3-TwoSum.java
@@ -15,3 +15,39 @@ class Solution {
         return twoSum;
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+        
+//         int upperLimitIndex = 0;
+        
+//         while (numbers[upperLimitIndex] < target) {
+//             upperLimitIndex++;
+            
+//             if (upperLimitIndex > numbers.length-2)
+//                 break;
+//         }
+        
+//         int p1 = 0;
+//         int p2 = p1+1;
+        
+//         int[] twoSum = new int[2];
+        
+//         while (twoSum[0] == 0 && twoSum[1] == 0) {
+//             while (p2 < upperLimitIndex) {
+//                 if (numbers[p1] + numbers[p2] == target) {
+//                     twoSum[0] = p1+1;
+//                     twoSum[1] = p2+1;
+//                     return twoSum;
+//                 }
+//                 p2++;
+//             }
+//             p1++;
+//             p2 = p1+1;
+//             if (p2 > numbers.length-1)
+//                 break;
+//         }
+        
+//         return twoSum;
+//     }
+// }
