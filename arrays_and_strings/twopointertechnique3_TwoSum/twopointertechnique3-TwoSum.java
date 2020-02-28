2d943c26c86c792e99121ff5502f49679d9b27f1
Ashwin Chalaka <ashesmachine@Ashwins-MacBook-Pro-2.local>
2020-02-27 16:17:36 -0800

    twopointertechnique3-TwoSum.java -- began algo, first trial ongoing

---
+++ arrays_and_strings/twopointertechnique3_TwoSum/twopointertechnique3-TwoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        
+        
+        for (int i = 0; i < numbers.length; i++) {
+            
+            for (int j = i; j < numbers.length; j++) {
+                
+                
+                
+            }
+            
+        }
+        
+    }
+}
