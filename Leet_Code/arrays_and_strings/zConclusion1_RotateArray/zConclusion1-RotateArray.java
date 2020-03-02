26b15341e2aa0a5e1b889310a8f9d6a969cdbc3b
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-02 14:58:08 -0800

    zConclusion1-RotateArray.java -- after many attempts, not able to create a simple algorithm for all cases (with this idea

--- Leet_Code/arrays_and_strings/zConclusion1_RotateArray/zConclusion1-RotateArray.java
+++ Leet_Code/arrays_and_strings/zConclusion1_RotateArray/zConclusion1-RotateArray.java
@@ -1,15 +1,62 @@
 class Solution {
+    private static void printNums(int[] nums) {
+        String str = "[";
+        
+        for (int i = 0; i < nums.length; i++) {
+            str += nums[i] + ",";
+        }
+        
+        str = str.substring(0,str.length()-1);
+        str += "]";
+        
+        System.out.println(str);
+    }
+    
     public void rotate(int[] nums, int k) {
         
-        int p1 = k-1;
-        int p2 = nums.length-1;
+        if (nums.length == 1)
+            return;
         
-        for (int i = 0; i < k; i++) {
+        if (k == 0)
+            return;
+        
+        int p1 = 0;
+        int p2 = nums.length-k;
+        
+        boolean p1IsValid;
+        
+        while (p1 < nums.length-k) {
+
+            // System.out.println(p1 + ", " + p2);
+
             nums[p1] += nums[p2];
             nums[p2] = nums[p1] - nums[p2];
             nums[p1] -= nums[p2];
-            p1--;
-            p2--;
-        }       
+
+            // printNums(nums);
+
+            p1++;
+            p2++;
+
+            if (p2 >= nums.length)
+                p2 = nums.length-k;
+        }
+        
+        if (nums.length % 2 == 1) {
+            nums[p1] += nums[p2];
+            nums[p2] = nums[p1] - nums[p2];
+            nums[p1] -= nums[p2];
+            
+            p1++;
+            p2++;
+
+            if (p2 >= nums.length)
+                p2 = nums.length-k;
+            
+            nums[p1] += nums[p2];
+            nums[p2] = nums[p1] - nums[p2];
+            nums[p1] -= nums[p2];
+        }
+        
     }
 }
