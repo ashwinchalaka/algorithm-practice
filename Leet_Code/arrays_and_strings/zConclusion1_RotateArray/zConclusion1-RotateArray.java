700c0e46e590086e233bb2bf3c3c775563eb3ab9
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-02 15:23:34 -0800

    zConclusion1-RotateArray.java -- FINISHED, working solution

--- Leet_Code/arrays_and_strings/zConclusion1_RotateArray/zConclusion1-RotateArray.java
+++ Leet_Code/arrays_and_strings/zConclusion1_RotateArray/zConclusion1-RotateArray.java
@@ -1,62 +1,16 @@
-class Solution {
-    private static void printNums(int[] nums) {
-        String str = "[";
-        
-        for (int i = 0; i < nums.length; i++) {
-            str += nums[i] + ",";
-        }
-        
-        str = str.substring(0,str.length()-1);
-        str += "]";
-        
-        System.out.println(str);
-    }
-    
+class Solution { 
     public void rotate(int[] nums, int k) {
         
-        if (nums.length == 1)
-            return;
+        int frontOfTheList;
         
-        if (k == 0)
-            return;
-        
-        int p1 = 0;
-        int p2 = nums.length-k;
-        
-        boolean p1IsValid;
-        
-        while (p1 < nums.length-k) {
+        for (int i = 0; i < k; i++) {
+            frontOfTheList = nums[nums.length-1];
 
-            // System.out.println(p1 + ", " + p2);
+            for (int j = nums.length-1; j > 0; j--) {
+                nums[j] = nums[j-1];
+            }
 
-            nums[p1] += nums[p2];
-            nums[p2] = nums[p1] - nums[p2];
-            nums[p1] -= nums[p2];
-
-            // printNums(nums);
-
-            p1++;
-            p2++;
-
-            if (p2 >= nums.length)
-                p2 = nums.length-k;
+            nums[0] = frontOfTheList;
         }
-        
-        if (nums.length % 2 == 1) {
-            nums[p1] += nums[p2];
-            nums[p2] = nums[p1] - nums[p2];
-            nums[p1] -= nums[p2];
-            
-            p1++;
-            p2++;
-
-            if (p2 >= nums.length)
-                p2 = nums.length-k;
-            
-            nums[p1] += nums[p2];
-            nums[p2] = nums[p1] - nums[p2];
-            nums[p1] -= nums[p2];
-        }
-        
     }
 }
