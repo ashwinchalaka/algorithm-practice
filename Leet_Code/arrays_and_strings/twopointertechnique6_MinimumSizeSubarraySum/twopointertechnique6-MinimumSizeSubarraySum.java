46f113b3adddc44424caa5bcd0de0d2535aaa483
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-02 13:46:54 -0800

    twopointertechnique6-MinimumSizeSubarraySum.java -- FINISHED, working solution

--- Leet_Code/arrays_and_strings/twopointertechnique6_MinimumSizeSubarraySum/twopointertechnique6-MinimumSizeSubarraySum.java
+++ Leet_Code/arrays_and_strings/twopointertechnique6_MinimumSizeSubarraySum/twopointertechnique6-MinimumSizeSubarraySum.java
@@ -10,8 +10,17 @@ class Solution {
         int subSum = 0;
         
         for (int i = 0; i < nums.length; i++) {
+            subSum += nums[i];
+        }
+        
+        if (subSum < s)
+            return 0;
+        
+        subSum = 0;
+        
+        for (int j = 0; j < nums.length; j++) {
             
-            int pointer = i;
+            int pointer = j;
             subLength = 0;
             subSum = 0;
             
