4ed71b9d1aefdb5b605dd8f3a7ce29ac3eec6fe0
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-13 14:16:11 -0700

    twopointertechnique-LinkedListCycle.java -- working for some cases, still debugging for other cases

--- Leet_Code/02-linked_list/twopointertechnique_LinkedListCycle/twopointertechnique-LinkedListCycle.java
+++ Leet_Code/02-linked_list/twopointertechnique_LinkedListCycle/twopointertechnique-LinkedListCycle.java
@@ -11,21 +11,50 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null)
-            return false;
+        ListNode slow1 = head;
+        ListNode fast1 = head;
         
-        ListNode walker1 = head;
-        ListNode walker2 = head;
+        if (slow1 == fast1.next)
+            return true;
         
-        while (walker1.next) {
-            walker2 = walker1;
-            while (walker2.next) {
-                // if (walker2.next == walker1)
-                //     return true;
-                walker2 = walker2.next;
-            }
-            walker1 = walker1.next;
+        int cycleCount = 0;
+        
+        while (cycleCount < 100) {
+            slow1 = slow1.next;
+            fast1 = fast1.next.next;
+            
+            if (slow1 == fast1)
+                return true;
+            
+            cycleCount++;
         }
+        
         return false;
+        
+//         if (head == null)
+//             return false;
+        
+//         ListNode walker1 = head;
+//         ListNode walker2 = head;
+        
+//         if (walker1.next != null) {
+//             System.out.println("1st: next not null");
+//             System.out.println(walker1.val + ", " + walker1.next.val + ", " + walker1.next.next.val);
+//         }
+        
+//         if (walker2.next.next != null)
+//             System.out.println("2nd: next next not null");
+        
+//         // while (walker1.next) {
+//         //     walker2 = walker1;
+//         //     while (walker2.next) {
+//         //         // if (walker2.next == walker1)
+//         //         //     return true;
+//         //         walker2 = walker2.next;
+//         //     }
+//         //     walker1 = walker1.next;
+//         // }
+        
+//         return false;
     }
 }
