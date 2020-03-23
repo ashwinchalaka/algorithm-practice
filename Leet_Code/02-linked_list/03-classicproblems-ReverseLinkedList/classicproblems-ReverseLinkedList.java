eba97f2ca8d49b72ea320f99f693f88eeb9fcabe
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-23 14:07:58 -0700

    classicproblems-ReverseLinkedList.java -- FINISHED, working solution

--- Leet_Code/02-linked_list/03-classicproblems-ReverseLinkedList/classicproblems-ReverseLinkedList.java
+++ Leet_Code/02-linked_list/03-classicproblems-ReverseLinkedList/classicproblems-ReverseLinkedList.java
@@ -8,6 +8,28 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
         
+        ListNode p0 = head;
+        ListNode p1 = head.next;
+        ListNode p2 = head.next.next;
+        // int count = 0;
+        
+        while (p2 != null) {
+            p0.next = p2;
+            p1.next = head;
+            head = p1;
+            p1 = p2;
+            // count++;
+            p2 = p2.next;
+        }
+        
+        p0.next = p2;
+        p1.next = head;
+        head = p1;
+        
+        // System.out.println(count);
+        return head;
     }
 }
