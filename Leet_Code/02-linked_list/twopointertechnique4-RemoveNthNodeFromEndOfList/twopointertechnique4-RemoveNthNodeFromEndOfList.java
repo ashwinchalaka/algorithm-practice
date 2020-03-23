771bb4d13404ac20f8ea0bd292587768f4b3622e
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-23 13:09:51 -0700

    twopointertechnique4-RemoveNthNodeFromEndOfList.java -- still working on solution, hoping my idea works

---
+++ Leet_Code/02-linked_list/twopointertechnique4-RemoveNthNodeFromEndOfList/twopointertechnique4-RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        ListNode p1 = head;
+        ListNode p2 = null;
+        int count = 0;
+        
+        while (p1 != null) {
+            count++;
+            
+            if (count == n)
+                p2 = head;
+                
+            p1 = p1.next;
+            if (p2 != null)
+                p2 = p2.next;
+        }
+        
+        if (p2 == null) {
+            head = head.next;
+        } else {
+            if (p2.next == null || p2.next.next == null)
+                p2.next = null;
+            else
+                p2.next = p2.next.next;
+        }
+        
+        return head;
+    }
+}
