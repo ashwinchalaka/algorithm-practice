e6f9ed0cdd73f1a98e5b5c0876efef99012f36c8
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-24 16:30:23 -0700

    classicproblems3-OddEvenLinkedList.java -- FINISHED, EVEN cleaner solution

--- Leet_Code/02-linked_list/03-classicproblems3-OddEvenLinkedList/classicproblems3-OddEvenLinkedList.java
+++ Leet_Code/02-linked_list/03-classicproblems3-OddEvenLinkedList/classicproblems3-OddEvenLinkedList.java
@@ -11,26 +11,18 @@ class Solution {
         if (head == null || head.next == null || head.next.next == null)
             return head;
         
-        ListNode head1 = head;
         ListNode head2 = head.next;
-        ListNode p1 = head1;
+        ListNode p1 = head;
         ListNode p2 = head2;
-        // boolean isOdd = true;
         
         while (p1.next != null && p2.next != null) {
-            // if (isOdd) {
-                // if (p2.next == null)
-                //     break;
                 p1.next = p1.next.next;
                 p1 = p1.next;
-            // } else {
                 p2.next = p2.next.next;
                 p2 = p2.next;
-            // }
-            // isOdd = !isOdd;
         }
         
         p1.next = head2;
-        return head1;
+        return head;
     }
 }
