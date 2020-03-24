9a4f74e0f1cc471599e79d1f175e33efbd18668c
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-24 16:16:16 -0700

    classicproblems3-OddEvenLinkedList.java -- FINISHED, working solution

--- Leet_Code/02-linked_list/03-classicproblems3-OddEvenLinkedList/classicproblems3-OddEvenLinkedList.java
+++ Leet_Code/02-linked_list/03-classicproblems3-OddEvenLinkedList/classicproblems3-OddEvenLinkedList.java
@@ -8,6 +8,29 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return head;
         
+        ListNode head1 = head;
+        ListNode head2 = head.next;
+        ListNode p1 = head1;
+        ListNode p2 = head2;
+        boolean isOdd = true;
+        
+        while (p1 != null && p2 != null) {
+            if (isOdd) {
+                if (p2.next == null)
+                    break;
+                p1.next = p2.next;
+                p1 = p1.next;
+            } else {
+                p2.next = p1.next;
+                p2 = p2.next;
+            }
+            isOdd = !isOdd;
+        }
+        
+        p1.next = head2;
+        return head1;
     }
 }
