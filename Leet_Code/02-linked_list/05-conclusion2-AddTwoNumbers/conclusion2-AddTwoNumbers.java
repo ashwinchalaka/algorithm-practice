974b9480e51007c3d9ed0cd75fbc90c16cefa2ef
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-03-25 16:18:26 -0700

    conclusion2-AddTwoNumbers.java -- completed a solution, but UNfinished because it is not the right task for this problem

--- Leet_Code/02-linked_list/05-conclusion2-AddTwoNumbers/conclusion2-AddTwoNumbers.java
+++ Leet_Code/02-linked_list/05-conclusion2-AddTwoNumbers/conclusion2-AddTwoNumbers.java
@@ -7,7 +7,48 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    private static ListNode reverseListWithLength(ListNode head) {
+        ListNode fastPointer = head;
+        ListNode slowPointer = null;
         
+        int length = 0;
+        
+        while (head != null) {
+            head = fastPointer.next;
+            fastPointer.next = slowPointer;
+            slowPointer = fastPointer;
+            fastPointer = head;
+            length++;
+        }
+        
+        ListNode node4Length = new ListNode(length);
+        node4Length.next = slowPointer;
+        
+        return node4Length;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode walkerfor1 = reverseListWithLength(l1);
+        int l1Length = walkerfor1.val;
+        walkerfor1 = walkerfor1.next;
+        
+        ListNode walkerfor2 = reverseListWithLength(l2);
+        int l2Length = walkerfor2.val;
+        walkerfor2 = walkerfor2.next;
+        
+        int sum = 0;
+        
+        while (walkerfor1 != null) {
+            sum += walkerfor1.val * Math.pow(10,l1Length-1);
+            walkerfor1 = walkerfor1.next;
+            l1Length--;
+        }
+        
+        while (walkerfor2 != null) {
+            sum += walkerfor1.val * Math.pow(10,l2Length-1);
+            walkerfor2 = walkerfor2.next;
+            l2Length--;
+        }
+        
+        return sum;
     }
 }
