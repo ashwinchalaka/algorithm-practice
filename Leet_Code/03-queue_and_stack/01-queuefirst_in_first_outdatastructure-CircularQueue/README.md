a29416768bacff701d8b193f22cc586d97c1b603
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-01 16:29:57 -0700

    queuefirst_in_first_outdatastructure-DesignCircularQueue.java -- FINISHED, working solution

--- Leet_Code/03-queue_and_stack/01-queuefirst_in_first_outdatastructure-CircularQueue/README.md
+++ Leet_Code/03-queue_and_stack/01-queuefirst_in_first_outdatastructure-CircularQueue/README.md
@@ -5,14 +5,6 @@ Filepath = algorithm-practice/Leet_Code/03-queue_and_stack/01-queuefirstinfirsto
 
 #### Problem Description
 
-
 #### Learnings
 
 #### Reference
-* Regarding,`ArrayList<Integer>` built-in method -- [ArrayList cheat sheet](https://www.geeksforgeeks.org/arraylist-in-java/)
-	* What I used for this problem
-		* `.get(int index)` -- returns Integer value for the element at the input index
-		* `.add(int value)` -- appends Integer value to the end of the ArrayList
-		* `.set(int index, E element)` -- overwrite an element in the ArrayList with a new element, at a specific index
-	* How to create an Integer value for the ^*.set* method -- [Integer cheat sheet](https://www.geeksforgeeks.org/java-lang-integer-class-java/)
-		* `Integer(int val)` -- constructor to initialize a value into an Integer object
--- Leet_Code/03-queue_and_stack/01-queuefirst_in_first_outdatastructure-CircularQueue/queuefirst_in_first_outdatastructure-DesignCircularQueue.java
+++ Leet_Code/03-queue_and_stack/01-queuefirst_in_first_outdatastructure-CircularQueue/queuefirst_in_first_outdatastructure-DesignCircularQueue.java
@@ -1,54 +1,86 @@
 class MyCircularQueue {
 
-    private ArrayList queue;
+    private int[] queueData;
     private int head;
     private int tail;
     
     /** Initialize your data structure here. Set the size of the queue to be k. */
     public MyCircularQueue(int k) {
-        queue = new ArrayList(k);
+        queueData = new int[k];
         head = -1;
         tail = -1;
+        System.out.println("constructor success");
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     public boolean enQueue(int value) {
-        if (head > -1 && head < queue.length) {
-            tail++;
-            if (tail == queue.length)
-                tail = 0;
-            if (queue[tail] != null)
-                queue[tail] = value;
-        } else if (head == -1) {
+        if (isFull()) {
+            return false;
+        } else if (isEmpty()) {
             head++;
             tail++;
-            queue[tail] = value;
+            queueData[tail] = value;
+        } else if (tail+1 != head && tail+1 < queueData.length) {
+            tail++;
+            queueData[tail] = value;
+        } else if (tail == queueData.length-1 && head > 0) {
+            tail = 0;
+            queueData[tail] = value;
+        } else {
+            System.out.println("New condition for enqueue: head = " + head + ", tail = " + tail + ",value = " + value);
         }
+        return true;
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
-        
+        if (isEmpty())
+            return false;
+        else if (head == tail) {
+            head = -1;
+            tail = -1;
+        } else if (head == queueData.length-1) {
+            head = 0;
+        } else if (head >= 0 && head < queueData.length-1) {
+            head++;
+        } else {
+            System.out.println("New condition for dequeue: head = " + head + ", tail = " + tail);
+        }
+        return true;
     }
     
     /** Get the front item from the queue. */
     public int Front() {
-        
+        if (isEmpty())
+            return head;
+        else
+            return queueData[head]; 
     }
     
     /** Get the last item from the queue. */
     public int Rear() {
-        
+        if (isEmpty())
+            return tail;
+        else
+            return queueData[tail];
     }
     
     /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
-        
+        if (head == -1 && tail == -1)
+            return true;
+        else
+            return false;
     }
     
     /** Checks whether the circular queue is full or not. */
     public boolean isFull() {
-        
+        if (head == 0 && tail == queueData.length-1)
+            return true;
+        else if (tail+1 == head)
+            return true;
+        else
+            return false;
     }
 }
 
