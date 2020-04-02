f0031d32c5aa082eee31bcd1ad8e6f873b02e4ab
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-02 16:24:50 -0700

    stack_last_in_first_out_data_structure-MinStack.java -- FINISHED, working solution

--- Leet_Code/03-queue_and_stack/03-stack_last_in_first_out_data_structure-MinStack/stack_last_in_first_out_data_structure-MinStack.java
+++ Leet_Code/03-queue_and_stack/03-stack_last_in_first_out_data_structure-MinStack/stack_last_in_first_out_data_structure-MinStack.java
@@ -1,24 +1,34 @@
 class MinStack {
-
+    List<Integer> stackData;
+    
     /** initialize your data structure here. */
     public MinStack() {
-        
+        stackData = new ArrayList<Integer>();
     }
     
     public void push(int x) {
-        
+        stackData.add(x);
     }
     
     public void pop() {
-        
+        stackData.remove(stackData.size()-1);
     }
     
     public int top() {
-        
+        return stackData.get(stackData.size()-1);
     }
     
     public int getMin() {
-        
+        if (stackData.size() == 1)
+            return stackData.get(0);
+        else {
+            int min = stackData.get(0);
+            for (int i = 1; i < stackData.size(); i++) {
+                if (stackData.get(i) < min)
+                    min = stackData.get(i);
+            }
+            return min;
+        }
     }
 }
 
