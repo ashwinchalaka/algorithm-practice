0e593a23ba975584dfc5896d5cbec307b253eb4a
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-08 18:07:34 -0700

    traverse_a_tree-BinaryTreePreorderTraversal.java -- trying a solution (round 2)

--- Leet_Code/04-binary_tree/01-traverse_a_tree-BinaryTreePreorderTraversal/traverse_a_tree-BinaryTreePreorderTraversal.java
+++ Leet_Code/04-binary_tree/01-traverse_a_tree-BinaryTreePreorderTraversal/traverse_a_tree-BinaryTreePreorderTraversal.java
@@ -11,20 +11,50 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> poList = new ArrayList<Integer>();
         Stack<TreeNode> stack4Nodes = new Stack<TreeNode>();
-        poList.add(root.val);
-        TreeNode walker = root;
+        
+        // poList.add(root.val);
+        System.out.println(root.val);
+        
+        TreeNode walker = root.left;
         
         while (walker.left != null) {
-            walker = walker.left;
             System.out.println(walker.val);
-            // poList.add(walker.val);
-            // stack4Nodes.push(walker);
+            if (walker.right != null)
+                stack4Nodes.add(walker);
+            walker = walker.left;
         }
         
         while (!stack4Nodes.empty()) {
             walker = stack4Nodes.pop();
+            while (walker.left != null) {
+                walker = walker.left;
+                System.out.println(walker.val);
+            }
+            while (walker.right != null) {
+                walker = walker.right;
+                System.out.println(walker.val);
+            }
+        }
+        
+        walker = root.right;
+        
+        while (walker.right != null) {
             System.out.println(walker.val);
-            // poList.add(walker.val)
+            if (walker.left != null)
+                stack4Nodes.add(walker);
+            walker = walker.right;
+        }
+        
+        while (!stack4Nodes.empty()) {
+            walker = stack4Nodes.pop();
+            while (walker.left != null) {
+                walker = walker.left;
+                System.out.println(walker.val);
+            }
+            while (walker.right != null) {
+                walker = walker.right;
+                System.out.println(walker.val);
+            }
         }
         
         return poList;
