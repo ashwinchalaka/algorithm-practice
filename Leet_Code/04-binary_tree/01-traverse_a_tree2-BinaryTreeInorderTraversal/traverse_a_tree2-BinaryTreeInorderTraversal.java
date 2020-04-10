1a88cd2feba70fa9060f48c06f283817b016f959
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-09 17:52:24 -0700

    traverse_a_tree2-BinaryTreeInorderTraversal.java -- making progress, working through the left side of the tree

--- Leet_Code/04-binary_tree/01-traverse_a_tree2-BinaryTreeInorderTraversal/traverse_a_tree2-BinaryTreeInorderTraversal.java
+++ Leet_Code/04-binary_tree/01-traverse_a_tree2-BinaryTreeInorderTraversal/traverse_a_tree2-BinaryTreeInorderTraversal.java
@@ -9,18 +9,38 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ioList = new ArrayList<Integer>;
+        List<Integer> ioList = new ArrayList<Integer>();
         
         if (root == null)
             return ioList;
         
         if (root.left != null) {
+            TreeNode walker = root.left;
             
+            Stack<TreeNode> stack4NodesLeft = new Stack<TreeNode>();
+            
+            while (walker != null) {
+                stack4NodesLeft.push(walker);
+                walker = walker.left;
+            }
+            
+            while (!stack4NodesLeft.empty()) {
+                walker = stack4NodesLeft.pop();
+                if (walker.left == null)
+                    System.out.println(walker.val);
+                if (walker.right != null)
+                    stack4NodesLeft.push(walker.right);
+                if (walker.left != null)
+                    stack4NodesLeft.push(walker.left);
+            }
         }
         
-        if (root.right != null) {
-            
-        }
+//         ioList.add(root.val);
+//         System.out.println(root.val);
+        
+//         if (root.right != null) {
+//             TreeNode walker = root.right;
+//         }
         
         return ioList;
     }
