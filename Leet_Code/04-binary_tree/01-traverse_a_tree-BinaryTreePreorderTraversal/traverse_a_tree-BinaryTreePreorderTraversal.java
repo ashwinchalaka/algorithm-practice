b9d40223e38e1643d3da5dfd4b1261be7ed8a9ab
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-09 17:20:33 -0700

    traverse_a_tree-BinaryTreePreorderTraversal.java -- FINISHED, working solution

--- Leet_Code/04-binary_tree/01-traverse_a_tree-BinaryTreePreorderTraversal/traverse_a_tree-BinaryTreePreorderTraversal.java
+++ Leet_Code/04-binary_tree/01-traverse_a_tree-BinaryTreePreorderTraversal/traverse_a_tree-BinaryTreePreorderTraversal.java
@@ -16,53 +16,56 @@ class Solution {
         
         Stack<TreeNode> stack4Nodes = new Stack<TreeNode>();
         
-        // poList.add(root.val);
-        System.out.println(root.val);
+        // System.out.println(root.val);
         poList.add(root.val);
         
-        TreeNode walker = root.left;
-        
-        while (walker != null) {
-            System.out.println(walker.val);
-            poList.add(walker.val);
-            if (walker.right != null) {
-                stack4Nodes.add(walker.right);
+        if (root.left != null) {
+            TreeNode walker = root.left;
+
+            while (walker != null) {
+                // System.out.println(walker.val);
+                poList.add(walker.val);
+                if (walker.right != null) {
+                    stack4Nodes.add(walker.right);
+                }
+                walker = walker.left;
             }
-            walker = walker.left;
+
+            while (!stack4Nodes.empty()) {
+                walker = stack4Nodes.pop();
+                // System.out.println(walker.val);
+                poList.add(walker.val);
+                if (walker.right != null) {
+                    stack4Nodes.add(walker.right);
+                }
+                if (walker.left != null) {
+                    stack4Nodes.add(walker.left);
+                }
+            }
         }
         
-        while (!stack4Nodes.empty()) {
-            walker = stack4Nodes.pop();
-            System.out.println(walker.val);
+        if (root.right != null) {
+            TreeNode walker = root.right;
+            // System.out.println(walker.val);
             poList.add(walker.val);
-            if (walker.right != null) {
-                stack4Nodes.add(walker.right);
-            }
-            if (walker.left != null) {
-                stack4Nodes.add(walker.left);
-            }
-        }
-        
-        walker = root.right;
-        
-        while (walker != null) {
-            System.out.println(walker.val);
-            poList.add(walker.val);
-            if (walker.left != null) {
-                stack4Nodes.add(walker.left);
-            }
-            walker = walker.right;
-        }
-        
-        while (!stack4Nodes.empty()) {
-            walker = stack4Nodes.pop();
-            System.out.println(walker.val);
-            poList.add(walker.val);
+
             if (walker.right != null) {
                 stack4Nodes.add(walker.right);
             }
             if (walker.left != null) {
                 stack4Nodes.add(walker.left);
+            }
+
+            while (!stack4Nodes.empty()) {
+                walker = stack4Nodes.pop();
+                // System.out.println(walker.val);
+                poList.add(walker.val);
+                if (walker.right != null) {
+                    stack4Nodes.add(walker.right);
+                }
+                if (walker.left != null) {
+                    stack4Nodes.add(walker.left);
+                }
             }
         }
         
