47d105133b4de036b7bc329c91fe10832509fb2f
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-17 15:11:09 -0700

    traverse_a_tree4-BinaryTreelevelorderTraversal.java -- getting somewhere, still need to work out the bugs

--- Leet_Code/04-binary_tree/1-traverse_a_tree4-BinaryTreelevelorderTraversal/traverse_a_tree4-BinaryTreelevelorderTraversal.java
+++ Leet_Code/04-binary_tree/1-traverse_a_tree4-BinaryTreelevelorderTraversal/traverse_a_tree4-BinaryTreelevelorderTraversal.java
@@ -11,28 +11,38 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         
         List<List<Integer>> loList = new ArrayList();
-        List<Integer> currentLevel = new ArrayList();
-        Queue queue4Nodes = new Queue();
         
         if (root.val == null)
             return loList;
-        else {
-            currentLevel.add(root.val);
-            if (root.left != null)
-                queue4Nodes.push(root.left);
-            if (root.right != null)
-                queue4Nodes.push(root.right);
-        }
+
+        List<Integer> currentLevel = new ArrayList();
+        Queue<Integer> queue4Nodes1 = new Queue<Integer>();
+        Queue<Integer> queue4Nodes2 = new Queue<Integer>();
         
+        currentLevel.add(root.val);
         loList.add(currentLevel);
         currentLevel = new ArrayList();
+
+        if (root.left != null)
+            queue4Nodes1.push(root.left);
+        if (root.right != null)
+            queue4Nodes1.push(root.right);
         
-        while (!queue4Nodes.empty()) {
+        while (!queue4Nodes1.empty() || !queue4Nodes2.empty()) {
+            
+            if (queue4Nodes1.empty()) {
+                loList.add(currentLevel);
+                currentLevel = newArrayList;
+                queue4Nodes1 = queue4Nodes2;
+                queue4Nodes2 = new Queue<Integer>();
+            }
+            
+            currentLevel.add(queue4Nodes1.remove().val);
             
             if (root.left != null)
-                queue4Nodes.push(root.left);
+                queue4Nodes2.push(root.left);
             if (root.right != null)
-                queue4Nodes.push(root.right);
+                queue4Nodes2.push(root.right);
         }
         
         return loList;
