53a3b2e3a6b2f865b63c37030a567e860a1db927
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-28 15:23:39 -0700

    solveproblemsrecursively2-SymmetricTree.java -- working through an idea [CONT.]

--- Leet_Code/04-binary_tree/2-solveproblemsrecursively2-SymmetricTree/solveproblemsrecursively2-SymmetricTree.java
+++ Leet_Code/04-binary_tree/2-solveproblemsrecursively2-SymmetricTree/solveproblemsrecursively2-SymmetricTree.java
@@ -9,6 +9,10 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        
+//      Etch Cases: (1) empty list
+//                  (2) root and no children
+//                  (3) children with different values
         if (root == null)
             return true;
         else if (root.left == null && root.right == null)
@@ -16,16 +20,34 @@ class Solution {
         else if (root.left.val != root.right.val)
             return false;
         
+//      If we're still not sure we need to compare one level at a time
         Queue<TreeNode> currentLevel = new LinkedList<>();
-        
         currentLevel.add(root.left);
         currentLevel.add(root.right);
         
         while (!currentLevel.isEmpty()) {
             
+            int clSize = currentLevel.size();
             
+            for (int i = 0; i < clSize; i=i+2) {
+//              poll() is a method like pop() but for LinkedList(), it removes ... 
+//              ... and returns the head of the list.
+                TreeNode left = currentLevel.poll();
+                TreeNode right = currentLevel.poll();
+
+                if (left.val != right.val)
+                    return false;
+                
+                
+            }
             
-            
+//          these are the children belonging to the next level
+//          pair #1: 
+            currentLevel.add(left.left);
+            currentLevel.add(right.right);
+
+            currentLevel.add(left.right);
+            currentLevel.add(right.left);
         }
         
         return true;
