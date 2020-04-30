282db219a79c72de05c65d3f998723538c70730e
Ashwin Chalaka <ashwin.chalaka@gmail.com>
2020-04-29 17:05:01 -0700

    conclusion1-ConstructBinaryTreeFromInorderAndPostorderTraversal.java -- working on solution

--- Leet_Code/04-binary_tree/3-conclusion1-ConstructBinaryTreeFromInorderAndPostorderTraversal/conclusion1-ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ Leet_Code/04-binary_tree/3-conclusion1-ConstructBinaryTreeFromInorderAndPostorderTraversal/conclusion1-ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -15,6 +15,22 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
+        if (inorder.length == postorder.length && inorder.length > 1) {
+            TreeNode root = new TreeNode(postorder[postorder.length-1]);
+            int ioRootLoc = 0;
+            int leftTreeLength = 0;
+            int rightTreeLength = 0;
+            
+            for (int i = 0; i < inorder.length; i++) {
+                
+            }
+
+            System.out.println(ioRootLoc + inorder[ioRootLoc]);
+            
+        } else if (inorder.length == 1 && postorder.length == 1) {
+            TreeNode root = new TreeNode(postOrder[0]);
+            return root;
+        } else
+            return null;
     }
 }
